9cd0c5d140eeb8c467d798f0c4ef570e67bb1aa8
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-02-13 13:07:19 +0530

    first commit

---
+++ daily_challenges/day-42/script.js
@@ -0,0 +1,7 @@
+// DOM - Document Object Model
+// javascript for Frontend Development
+// DOM has 4 pillars
+// 1. Selection of an Element
+// 2. Chaning HTML
+// 3. Changing CSS
+// 4. Event Listeners
