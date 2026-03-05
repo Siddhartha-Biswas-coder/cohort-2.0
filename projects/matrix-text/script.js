b20dc6fdf2beed1bb7510634ba3a81cf45934233
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-03-05 19:11:59 +0530

    matrix text

--- projects/matrix-text/script.js
+++ projects/matrix-text/script.js
@@ -1,16 +1,67 @@
-const para = document.querySelector("p")
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-const text = para.innerText
+// const para = document.querySelector("p")
+// const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+// const text = para.innerText
 
-para.addEventListener('mouseenter',function(){
+// let iteration = 0 
+// let interval;
 
-    setInterval(()=> {
-        
-        const str = text.split('').map((char,index)=>{
-            return characters[Math.floor(Math.random()*characters.length)]
-        }).join('')
 
-        para.innerText = str
+// function randomText (){
+//     const str = text.split('').map((char,index)=>{
 
-    },30)
-})
+//             if (index < iteration){
+//                 return char
+//             }
+//             return characters[Math.floor(Math.random()*characters.length)]
+//         }).join('')
+
+//         para.innerText = str
+//         iteration += 1
+
+//         if(iteration > text.length){
+//             clearInterval(interval)
+//         }
+// }
+
+// para.addEventListener('mouseenter',function(){
+
+//     iteration = 0;
+//     clearInterval(interval);
+
+//     interval = setInterval(randomText,30)
+// })
+
+const para = document.querySelector("p");
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const text = para.innerText;
+
+let iteration = 0;
+let interval;
+
+para.addEventListener("mouseenter", () => {
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+
+        para.innerText = text
+            .split("")
+            .map((char, index) => {
+
+                if (index < iteration) {
+                    return text[index];
+                }
+
+                return characters[Math.floor(Math.random() * characters.length)];
+            })
+            .join("");
+
+        iteration += 0.2;
+
+        if (iteration > text.length) {
+            clearInterval(interval);
+        }
+
+    }, 30);
+
+});
