d320dd8f3f1f1d11799a498dbdf489815381c0bc
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-03-07 20:42:10 +0530

    OOPS

---
+++ daily_challenges/advance-javascript/object-oriented-programming/script.js
@@ -0,0 +1,84 @@
+// objects
+// let user1 = {
+//     name: "Siddhartha Biswas",
+//     age : 21,
+//     email : 'siddharthabiswas2024@gmail.com',
+// }
+// let user2 = {
+//     name: "Soham Sen",
+//     age : 21,
+//     email : 'sohamsen2024@gmail.com',
+// }
+
+
+//classes
+// class Remote{
+//     constructor(){
+//         this.product = "daikin";
+//         this.price = 12300;
+//         this.color = "white";
+//     }
+
+//     powerOn(){
+//         console.log("The machine is on now.")
+//     }
+//     powerOff(){
+//         console.log("The machine is off now.")
+//     }
+// }
+
+// let remote = new Remote()
+// class Remote{
+//     constructor(product,price,color){
+//         this.product = product;
+//         this.price = price;
+//         this.color = color;
+//     }
+
+//     powerOn(){
+//         console.log("The machine is on now.")
+//     }
+//     powerOff(){
+//         console.log("The machine is off now.")
+//     }
+// }
+
+// let remote1 = new Remote("daikin",12300,"white")
+// let remote2 = new Remote("lenovo",12110,"red")
+
+// class BiscuitMaker{
+//     constructor(){
+//         this.name = "ParleG";
+//         this.price = 5;
+//     }
+// }
+
+// class er made so that we can get a new object evrytime
+// on running new with a class , a new oject is created
+
+// class Book{
+//     constructor(name,price,author,color){
+//         this.name= name;
+//         this.price = price;
+//         this.author = author;
+//         this.color = color
+//     }
+
+//     readBook(){}
+//     bookmark(){}
+
+// }
+
+// class User{
+//     constructor(){
+//         this.username = "Siddhartha";
+//         this.age = 21;
+//         this.email = "sfnsdb"
+//     }
+
+//     eatFood(){}
+//     breathe(){}
+// }
+
+// let book1 = new Book('moral',120,'Siddhartha','white')
+// let book2 = new Book('computer science',890,'Soham','red')
