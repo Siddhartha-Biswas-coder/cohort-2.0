8d3a85818357118bb52405b460afeba4bfc71e10
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-03-08 01:13:26 +0530

    this keyword

--- daily_challenges/advance-javascript/object-oriented-programming/day-57/script.js
+++ daily_challenges/advance-javascript/object-oriented-programming/day-57/script.js
@@ -1,13 +1,65 @@
-class Sketch{
-    constructor(character,color){
-        this.character = character;
-        this.color = color;
-        this.someFunc = function(){};
-    }
-}
+// class Sketch{
+//     constructor(character,color){
+//         this.character = character;
+//         this.color = color;
+//         this.someFunc = function(){};
+//     }
+// }
 
-Sketch.prototype.speak = function(){}
-Sketch.prototype.walk = function(){}
+// Sketch.prototype.speak = function(){}
+// Sketch.prototype.walk = function(){}
+
+// let sketch1 = new Sketch("Doremon","blue");
 
 
-let sketch1 = new Sketch("Doremon","blue");
+// global -> window
+// console.log(this); //window
+
+// function -> window
+// function abcd() {
+//   console.log(this);
+// }
+// abcd(); // window
+
+// es5 function inside object -> object
+// let obj1 = {
+//   name: "siddhartha",
+//   func: function () {
+//     console.log(this); //obj1
+//   },
+// };
+// obj1.func();
+
+// es6 function inside object -> window
+// let obj4 = {
+//     fuc: () => {
+//         console.log(this) //window
+//     }
+// }
+// obj4.fuc()
+
+// es5 function inside es5 function inside object -> window
+// let obj2 = {
+//   name: "siddhartha",
+//   func: function () {
+//     function def() {
+//       console.log(this); //window
+//     }
+//     def();
+//   },
+// };
+// obj2.func();
+
+// es6 function inside es5 function inside object -> object
+// let obj3 = {
+//   name: "siddhartha",
+//   func: function () {
+//     let def = () => {
+//       console.log(this); //obj3
+//     };
+//     def();
+//   },
+// };
+// obj3.func();
+
+
