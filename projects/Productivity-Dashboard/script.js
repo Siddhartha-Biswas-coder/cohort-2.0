4d2b5707c0bbdbac7e17597c74fd429115872c46
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-03-12 02:08:17 +0530

    dashboard-project

--- projects/Productivity-Dashboard/script.js
+++ projects/Productivity-Dashboard/script.js
@@ -1,9 +1,19 @@
-var allElems = document.querySelectorAll(".all-Elements .elements");
-var allFullElems = document.querySelectorAll(".fullElem");
+function openFeatures() {
+  var allElems = document.querySelectorAll(".all-Elements .elements");
+  var fullElemsPage = document.querySelectorAll(".fullElem");
+  var fullElemsPageBackBtn = document.querySelectorAll(".fullElem .back");
 
-allElems.forEach(function (elem) {
-  elem.addEventListener("click", function () {
-    console.log(allFullElems[elem.id]);
-    allFullElems[elem.id].style.display = "block";
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      fullElemsPage[elem.id].style.display = "block";
+    });
   });
-});
+
+  fullElemsPageBackBtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      // console.log(back.id)
+      fullElemsPage[back.id].style.display = "none";
+    });
+  });
+}
+openFeatures()
