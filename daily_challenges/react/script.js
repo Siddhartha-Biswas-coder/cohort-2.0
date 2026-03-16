94b9ba77a86f75969e95810e69d1f50920b18ad8
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-03-16 20:34:14 +0530

    basic React

---
+++ daily_challenges/react/script.js
@@ -0,0 +1,6 @@
+var h1 = React.createElement("h1", null, "Hey I am Siddhartha ");
+
+var container = document.querySelector("#container");
+
+var root = ReactDOM.createRoot(container);
+root.render(h1)
