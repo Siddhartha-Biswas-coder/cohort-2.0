262222b05051147914ef5c00ed571f1cb3095a90
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-03-17 22:41:03 +0530

    learned Tailwind css

---
+++ daily_challenges/react/class2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text">Siddhartha Biswas is a God</h1>
+      <h2 className="bg-red-500 h-25 ">He is a God </h2>
+    </div>
+  );
+};
+
+export default App;
