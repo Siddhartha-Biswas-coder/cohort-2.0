6e49fabb8caca975704440113d624f9552e4e493
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-05-06 17:48:55 +0530

    mouse follower

---
+++ projects/mouse-follower/script.js
@@ -0,0 +1,25 @@
+const mouseFollower = document.querySelector(".mouse-follower");
+
+let x = 0,
+  y = 0;
+
+addEventListener("mousemove", (e) => {
+  const { clientX, clientY } = e;
+
+  //   mouseFollower.style.top = clientY + 'px'
+  //   mouseFollower.style.left = clientX + 'px'
+
+  //   oprimised
+  //   mouseFollower.style.transform = `translate(${clientX}px,${clientY}px)`;
+
+  x = clientX;
+  y = clientY;
+});
+
+function far() {
+  mouseFollower.style.transform = `translate(${x}px,${y}px)`;
+
+  requestAnimationFrame(far);
+}
+
+far();
