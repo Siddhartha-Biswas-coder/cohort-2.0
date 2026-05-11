73409d5fbf14247cb7db073a4c0d300272e9cba8
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-05-11 14:28:38 +0530

    day-7 backend

---
+++ backend/day-7/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const app = require("./src/app.js");
+
+const connectToDb = require("./src/config/database.js");
+
+connectToDb();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ backend/day-7/src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const noteModel = require("./models/notes.model.js");
+const app = express();
+
+app.use(express.json());
+
+/**
+ * - POST /notes
+ * - req.body => {title,description}
+ */
+app.post("/notes", async (req, res) => {
+  const { title, description, age } = req.body;
+
+  const note = await noteModel.create({
+    title,
+    description,
+    age,
+  });
+
+  res.status(201).json({
+    message: "Note created successfully",
+    note,
+  });
+});
+
+module.exports = app;
