a588f3963893d67675631dd7a807195b681a2008
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-05-18 02:07:27 +0530

    add follow model and controller

--- backend/day-12 (instagram clone)/src/routes/post.routes.js
+++ backend/day-12 (instagram clone)/src/routes/post.routes.js
@@ -3,6 +3,7 @@ const postRouter = express.Router();
 const postController = require("../controllers/post.controller.js");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
+const identifyUser = require("../middlewares/auth.middleware.js");
 
 /**
  * POST /api/posts [protected]
@@ -12,6 +13,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 postRouter.post(
   "/",
   upload.single("image"),
+  identifyUser,
   postController.createPostController,
 );
 
@@ -19,13 +21,17 @@ postRouter.post(
  * GET /api/posts/
  */
 
-postRouter.get("/", postController.getPostController);
+postRouter.get("/", identifyUser, postController.getPostController);
 
 /**
  * GET api/posts/details/:postid
  * - return an detail about specific post with the id, also check whether the post belongs to the user that is requesting
  */
 
-postRouter.get("/details/:postId",postController.getPostDetailsController)
+postRouter.get(
+  "/details/:postId",
+  identifyUser,
+  postController.getPostDetailsController,
+);
 
 module.exports = postRouter;
