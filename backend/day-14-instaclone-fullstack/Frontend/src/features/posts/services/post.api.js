a63cd7177dc781ba84490c3538de9d47968184f7
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-05-22 23:18:50 +0530

    feat: update API base URLs to use production environment

--- backend/day-14-instaclone-fullstack/Frontend/src/features/posts/services/post.api.js
+++ backend/day-14-instaclone-fullstack/Frontend/src/features/posts/services/post.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://siddhartha-instaclone-api.onrender.com",
   withCredentials: true,
 });
 
