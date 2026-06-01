307a89a0debddae1305f88706653cf3d9bbf2d94
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-06-02 00:41:09 +0530

    feat: implement chat functionality with Redux integration

--- backend/day-20-PERPLEXITY/Backend/src/controllers/chat.controller.js
+++ backend/day-20-PERPLEXITY/Backend/src/controllers/chat.controller.js
@@ -23,7 +23,7 @@ export async function sendMessage(req, res) {
     role: "user",
   });
 
-  const messages = await messageModel.find({ chat: chatId });
+  const messages = await messageModel.find({ chat: chatId || chat._id });
 
   const result = await generateResponse(messages);
 
@@ -98,4 +98,3 @@ export async function deleteChat(req, res) {
     message: "chat deleted successfully",
   });
 }
-
