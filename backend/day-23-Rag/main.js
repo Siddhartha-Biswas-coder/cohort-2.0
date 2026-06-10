7656a5d4c9d1494e7a9a65a80b0d69034a75d1ac
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-06-11 03:56:21 +0530

    chore: implemented RAG and Vector Database

---
+++ backend/day-23-Rag/main.js
@@ -0,0 +1,63 @@
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
+const index = pc.index("cohort-2-rag");
+
+// const loader = new PDFLoader("./story.pdf");
+
+// const data = await loader.load();
+
+const embeddings = new MistralAIEmbeddings({
+  apiKey: process.env.MISTRAL_API_KEY,
+  model: "mistral-embed",
+});
+
+// const splitter = new RecursiveCharacterTextSplitter({
+//   chunkSize: 500,
+//   chunkOverlap: 0,
+// });
+
+// const chunks = await splitter.splitDocuments(docs);
+// const chunks = await splitter.splitText(
+//   data.map((doc) => doc.pageContent).join("\n"),
+// );
+
+// const docs = await Promise.all(
+//   chunks.map(async (chunk) => {
+//     const embedding = await embeddings.embedQuery(chunk);
+//     return {
+//       text: chunk,
+//       embedding,
+//     };
+//   }),
+// );
+
+// const result = await index.upsert({
+//   records: docs.map((doc, i) => ({
+//     id: `doc-${i}`,
+//     values: doc.embedding,
+//     metadata: {
+//       text: doc.text,
+//     },
+//   })),
+// });
+
+const queryEmbedding = await embeddings.embedQuery(
+  "How was the internship experience?",
+);
+
+console.log(queryEmbedding);
+
+const result = await index.query({
+  vector: queryEmbedding,
+  topK: 2,
+  includeMetadata: true,
+});
+
+console.log(JSON.stringify(result));
