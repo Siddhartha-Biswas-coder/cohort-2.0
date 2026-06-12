c47daadd059765b890be2c5b9264086f6280e307
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-06-12 17:28:33 +0530

    feat: initialize LangGraph service and integrate into Express application routes

--- backend/day-25-LangGraph-battle-ai-arena/src/services/graph.ai.service.ts
+++ backend/day-25-LangGraph-battle-ai-arena/src/services/graph.ai.service.ts
@@ -1,31 +1,36 @@
+import { HumanMessage } from "@langchain/core/messages";
 import {
   StateSchema,
   MessagesValue,
+  ReducedValue,
   StateGraph,
   START,
   END,
 } from "@langchain/langgraph";
+import type { GraphNode } from "@langchain/langgraph";
 
-type JUDGEMENT = {
-  winner: "solution_1" | "solution_2";
-  solution_1_score: number;
-  solution_2_score: number;
-};
-
-type AIBATTLESTATE = {
-  messages: typeof MessagesValue;
-  solution_1: string;
-  solution_2: string;
-  judgement: JUDGEMENT;
-};
-
-const state: AIBATTLESTATE = {
+const State = new StateSchema({
   messages: MessagesValue,
-  solution_1: "",
-  solution_2: "",
-  judgement: {
-    winner: "solution_1",
-    solution_1_score: 0,
-    solution_2_score: 0,
-  },
+});
+
+const solutionNode: GraphNode<typeof State> = (
+  state: typeof State,
+): typeof State => {
+  console.log(state.messages);
+  return {
+    messages: state.messages[0],
+  };
 };
+
+const graph = new StateGraph(State)
+  .addNode("solution", solutionNode)
+  .addEdge(START, "solution")
+  .compile();
+
+export default async function (userMessage: string) {
+  const result = await graph.invoke({
+    messages: [new HumanMessage(userMessage)],
+  });
+
+  return result.messages;
+}
