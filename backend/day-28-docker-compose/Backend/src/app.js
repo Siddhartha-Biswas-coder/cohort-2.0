9c229cca3e8023e7a48ff26954a7db57a337fa53
Siddhartha Biswas <siddharthabiswas2024@gmail.com>
2026-07-12 12:28:51 +0530

    feat: configure docker containerization and static asset structure for backend deployment

--- backend/day-28-docker-compose/Backend/src/app.js
+++ backend/day-28-docker-compose/Backend/src/app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.static("public"));
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ msg: "success" });
@@ -16,6 +17,10 @@ app.get("/api/users", (req, res) => {
     { id: 3, name: "charlie" },
   ];
   res.status(200).json({ users });
+});
+
+app.get("*name", (req, res) => {
+  res.sendFile("public/index.html", { root: __dirname });
 });
 
 app.get("/api/hello", (req, res) => {
